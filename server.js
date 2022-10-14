aab309d50303af6c51fd94af96c259603e604c9c
JG <gaojunqi@outlook.com>
2022-10-14 14:19:34 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
-    originWhitelist: ['http://localhost:3000', 'https://ijg.io'],
+    originWhitelist: ['http://localhost:3000', 'https://xxrr.top', 'http://xxrr.top'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
